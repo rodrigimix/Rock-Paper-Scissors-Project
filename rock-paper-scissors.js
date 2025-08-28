795fe7b9dce539812701ff18173efe47235ebf08
Rodrigo Marques <rodrigimix@protonmail.com>
2025-08-28 21:52:24 +0100

    Step 2 Done
    Added the logic to get the computer choose between rock, paper or scissors.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+
+function getComputerChoice() {
+  const computerChoice = getRandomInt(3);
+  console.log(computerChoice);
+  if (computerChoice == 0) {
+    return "rock";
+  } else if (computerChoice == 1) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice())
